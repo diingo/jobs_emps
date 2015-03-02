0314883cdf166209027d57cffdad4121e53ea763
Gene Le <gene.gkl@gmail.com>
2015-03-01 18:13:01 -0600

    Add model methods for Job and JobsEmployee

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,28 @@
 class Job < ActiveRecord::Base
   has_many :jobs_employees
   has_many :employees, through: :jobs_employees
+
+  def self.create_with_params(job_params, params)
+    job = Job.new(job_params)
+
+    job.add_users(params)
+
+    job.save
+    job
+  end
+
+  def self.update_with_params(job, job_params, params)
+    job.add_users(params)
+
+    job.update_attributes(job_params)
+  end
+
+  def add_users(params)
+    manager_id = params[:job][:manager_id]
+    executive_id = params[:job][:executive_id]
+    self.employee_ids = [manager_id, executive_id]
+    # Just in case something weird happens:
+    self.employee_ids = self.employee_ids.reject(&:blank?)
+    self.employee_ids = self.employee_ids.uniq
+  end
 end
--- app/models/jobs_employee.rb
+++ app/models/jobs_employee.rb
@@ -1,2 +1,17 @@
 class JobsEmployee < ActiveRecord::Base
+  belongs_to :employee
+  belongs_to :job
+
+  before_save :set_employee_type
+
+  def set_employee_type
+    self.employee_type ||= self.employee.type
+  end
+
+  # Find all employees of a certain type (e.g. crew member) for a specific job
+  def self.find_employees(job_id, employee_type)
+    self.where(job_id: job_id, employee_type: employee_type).map do |job_emp|
+      Employee.find(job_emp.employee_id)
+    end
+  end
 end
